e0691122a9adfe2b173b4e3138d30fcdc3fe8b9d
shaik Ashraf Ali <101001875+ashrafalih@users.noreply.github.com>
2023-12-06 10:23:04 +0530

    updated for 7th parameter to pass

--- PythonScripts/triggerscripts.py
+++ PythonScripts/triggerscripts.py
@@ -8,10 +8,12 @@ REPO = str(sys.argv[3])
 Workflow_Name = str(sys.argv[4])
 parameter1 = str(sys.argv[5])
 parameter2 = str(sys.argv[6])
+# adding below extra line for testing 
+image_name_tag = str(sys.argv[7])
 
 print("The token value is")
 
-def trigger_workflow(workflow_name, parameter1, parameter2):
+def trigger_workflow(workflow_name, parameter1, parameter2, image_name_tag):
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization": f"token {TOKEN}",
@@ -21,7 +23,8 @@ def trigger_workflow(workflow_name, parameter1, parameter2):
         "event_type": Workflow_Name,  
         "client_payload": {  # this is the way to get the values in workflow1 to workflow2
             'parameter1': parameter1,
-            'parameter2': parameter2  
+            'parameter2': parameter2,
+            'image_name_tag': image_name_tag 
         }
     }
 
@@ -31,4 +34,4 @@ def trigger_workflow(workflow_name, parameter1, parameter2):
     # responseValue = requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches", json=data, headers=headers)
     print("The response message is ", responseValue.content)  
 
-trigger_workflow(Workflow_Name, parameter1, parameter2) 
+trigger_workflow(Workflow_Name, parameter1, parameter2, image_name_tag) 
